75c5873dab28ed9d9e06a6e4172ba5e12f60771b
Darshit Shah <darshit@yuktianalytics.com>
2015-12-20 08:29:14 +0530

    Version 2

--- model.js
+++ model.js
@@ -2,9 +2,76 @@
 
 var fs = require('fs');
 var lpsolve = require('lp_solve');
-var demand = readCsvFile('Demand.csv', 3, true);
-var supply = readCsvFile('Supply.csv', 3, true);
-//console.log(demand, supply);
+
+var utils={
+    readCSVFile: function(path, fieldsLength, convertUpper){
+        var data = fs.readFileSync(path);
+        var rows = data.toString().split("\r\n")
+        for (var rIndex = 0; rIndex < rows.length; rIndex++) {
+            if(rows[rIndex].length>0){
+                rows[rIndex]=rows[rIndex].split(",");
+                if(rows[rIndex].length != fieldsLength){
+                    console.log(fieldsLength," fields required. Found: ",rows[rIndex].length, "Data: ", rows[rIndex]);
+                    rows.splice(rIndex, 1);
+                    rIndex--;
+                }
+                else {
+                    if(convertUpper!=false){
+                        for (var fIndex = 0; fIndex < rows[rIndex].length; fIndex++) {
+                            rows[rIndex][fIndex] = rows[rIndex][fIndex].toUpperCase();
+                        }
+                    }
+                }
+            }
+            else {
+                rows.splice(rIndex, 1);
+                rIndex--;
+            }
+        }
+        return rows;
+    },
+    JSON2CSV: function (objArray, includeHeader) {
+        var array = typeof objArray != 'object' ? [objArray] : objArray;
+        //console.log(typeof objArray);
+        var str = '';
+        var tempData = [];
+        if (array.length > 0) {
+            var keys = Object.keys(array[0]);
+            if (includeHeader != false) {
+                //str += keys.join(',') + '\r\n';
+                tempData.push(keys.join(','));
+            }
+            //   console.log('JSON2CSV called for Array Size:', objArray.length, ' and keys size: ', keys.length);
+            //append data
+            for (var k = 0; k < array.length; k++) {
+                var line = [];
+                for (var index = 0; index < keys.length; index++) {
+                    if (array[k].hasOwnProperty(keys[index])) {
+                        var val = array[k][keys[index]];
+                        if (typeof val == 'string' && val != null) {
+                            if (val.indexOf(',') != -1)
+                                line.push('"' + val + '"');
+                            else
+                                line.push(val);
+                        }
+                        else if (Object.prototype.toString.call(val) === '[object Date]') {
+                            line.push(dateTimeFormat.formatDate(val, "yyyy-MM-dd HH:mm:ss"));
+                        }
+                        else {
+                            line.push(val);
+                        }
+                    }
+                }
+                //str += line.join(',') + '\r\n';
+                tempData.push(line.join(','));
+            }
+            return tempData.join('\r\n') + '\r\n';
+        }
+    }
+}
+
+var demand = utils.readCSVFile('demand.csv', 4, true);
+var supply = utils.readCSVFile('supply.csv', 4, true);
 
 var Row = lpsolve.Row;
 
@@ -15,13 +82,13 @@ var lp = new lpsolve.LinearProgram();
 var demandKeys=[];
 var demandValues=[];
 for (var dIndex = 1; dIndex < demand.length; dIndex++) {
-    var localIndex = demandKeys.indexOf(demand[dIndex][0]);
+    var localIndex = demandKeys.indexOf(demand[dIndex][1]);
     if(localIndex == -1){
-        demandKeys.push(demand[dIndex][0]);
-        demandValues.push({ key: demandKeys[demandKeys.length-1], demand: parseInt(demand[dIndex][2]), decisionVariables: [] });
+        demandKeys.push(demand[dIndex][1]);
+        demandValues.push({ key: demandKeys[demandKeys.length-1], demand: parseInt(demand[dIndex][3]), decisionVariables: [] });
     }
     else {
-        demandValues[localIndex].demand += parseInt(demand[dIndex][2]);
+        demandValues[localIndex].demand += parseInt(demand[dIndex][3]);
     }
 }
 
@@ -29,8 +96,8 @@ for (var dIndex = 1; dIndex < demand.length; dIndex++) {
 var demandMGKeys=[];
 var demandMGValues=[];
 for (var dIndex = 1; dIndex < demand.length; dIndex++) {
-    demandMGKeys.push(demand[dIndex][0]+"_"+demand[dIndex][1]);
-    demandMGValues.push({ key: demandMGKeys[demandMGKeys.length-1], demand: parseInt(demand[dIndex][2]), decisionVariables: [] });
+    demandMGKeys.push(demand[dIndex][1]+":::"+demand[dIndex][2]);
+    demandMGValues.push({ key: demandMGKeys[demandMGKeys.length-1], demand: parseInt(demand[dIndex][3]), decisionVariables: [] });
 }
 
 var decisionVariables=[];
@@ -39,27 +106,28 @@ var operatorKeys=[];
 var operatorValues=[];
 
 for (var i = 1; i < supply.length; i++) {
-    var operatorIndex = operatorKeys.indexOf(supply[i][0]+'_'+supply[i][1]);
+    var operatorIndex = operatorKeys.indexOf(supply[i][1]+":::"+supply[i][2]);
     if(operatorIndex == -1){
-        operatorKeys.push(supply[i][0]+'_'+supply[i][1]);
-        operatorValues.push({ key: operatorKeys[operatorKeys.length-1], supply: supply[i][2], decisionVariables: [] });
+        operatorKeys.push(supply[i][1]+":::"+supply[i][2]);
+        operatorValues.push({ key: operatorKeys[operatorKeys.length-1], supply: supply[i][3], decisionVariables: [] });
         operatorIndex = operatorKeys.length-1;
     }
 
-    var demandIndex = demandKeys.indexOf(supply[i][0]);
+    var demandIndex = demandKeys.indexOf(supply[i][1]);
     if(demandIndex == -1){
-        demandKeys.push(supply[i][0]);
+        demandKeys.push(supply[i][1]);
         demandValues.push({ key: demandKeys[demandKeys.length-1], demand: 0, decisionVariables: [] });
+        demandIndex = demandKeys.length-1;
     }
     for (var iMG = 1; iMG < 4; iMG++) {
-        var demandMGIndex = demandMGKeys.indexOf(supply[i][0]+"_MG"+iMG);
+        var demandMGIndex = demandMGKeys.indexOf(supply[i][1]+":::MG"+iMG);
         if(demandMGIndex == -1){
-            demandMGKeys.push(supply[i][0]+"_MG"+iMG);
+            demandMGKeys.push(supply[i][1]+":::MG"+iMG);
             demandMGValues.push({ key: demandMGKeys[demandMGKeys.length-1], demand: 0, decisionVariables: [] });
             demandMGIndex = demandMGKeys.length-1;
         }
         for (var iSG = 1; iSG < 3; iSG++) {
-            var variableName = supply[i][0]+'_'+supply[i][1]+'_'+'MG'+iMG+'_SG'+iSG;//C1_O1_MG1_SG1
+            var variableName = supply[i][1]+":::"+supply[i][2]+":::"+"MG"+iMG+":::SG"+iSG;//C1_O1_MG1_SG1
             var decisionVariable = lp.addColumn(variableName, true);
             //objective = objective.Add(decisionVariable, 1);
             decisionVariables.push({key:variableName, value:decisionVariable});
@@ -72,7 +140,8 @@ for (var i = 1; i < supply.length; i++) {
 }
 
 for (var dIndex = 0; dIndex < demandMGValues.length; dIndex++) {
-    var cIndex = demandKeys.indexOf(demandMGValues[dIndex].key.split("_")[0]);
+    console.log(demandKeys, demandMGValues[dIndex].key)
+    var cIndex = demandKeys.indexOf(demandMGValues[dIndex].key.split(":::")[0]);
     demandMGValues[dIndex].demandPerc = (1.0 * demandMGValues[dIndex].demand) / demandValues[cIndex].demand;
 }
 
@@ -95,20 +164,22 @@ for (var dIndex = 0; dIndex < operatorKeys.length; dIndex++) {
     if (operatorValues[dIndex].decisionVariables.length > 0) {
         for (var demandMGIndex = 0; demandMGIndex < demandMGKeys.length; demandMGIndex++) {
             var MGConstraint = new Row();
-            var demandMGKeyParts = demandMGKeys[demandMGIndex].split("_");
+            var MGConstraintAdded=false;
+            var demandMGKeyParts = demandMGKeys[demandMGIndex].split(":::");
             var value = operatorValues[dIndex].supply * demandMGValues[demandMGIndex].demandPerc;
             var innerConstraints = new Row();
+            var innerConstraintsAdded = false;
             for (var j = 0; j < operatorValues[dIndex].decisionVariables.length; j++) {
-                if(operatorValues[dIndex].decisionVariables[j].indexOf(demandMGKeyParts[0]+"_")>-1
-                && operatorValues[dIndex].decisionVariables[j].indexOf("_"+demandMGKeyParts[1])>-1){
+                if(operatorValues[dIndex].decisionVariables[j].indexOf(demandMGKeyParts[0]+":::")>-1
+                && operatorValues[dIndex].decisionVariables[j].indexOf(":::"+demandMGKeyParts[1])>-1){
                     operatorConstraint[dIndex] = operatorConstraint[dIndex].Add(operatorValues[dIndex].decisionVariables[j], 1);
                     MGConstraint = MGConstraint.Add(operatorValues[dIndex].decisionVariables[j], 1);
-
+                    MGConstraintAdded=true;
                     var SGValue = 0;
-                    if(operatorValues[dIndex].decisionVariables[j].indexOf("_SG1")>-1){
+                    if(operatorValues[dIndex].decisionVariables[j].indexOf(":::SG1")>-1){
                         SGValue = value * 0.45;
                     }
-                    else if(operatorValues[dIndex].decisionVariables[j].indexOf("_SG2")>-1){
+                    else if(operatorValues[dIndex].decisionVariables[j].indexOf(":::SG2")>-1){
                         SGValue = value * 0.55;
                     }
                     else {
@@ -119,17 +190,23 @@ for (var dIndex = 0; dIndex < operatorKeys.length; dIndex++) {
                     lp.addConstraint(SGConstraint, 'GE', Math.floor(SGValue), 'MG (' + operatorValues[dIndex].key + ')');
                     lp.addConstraint(SGConstraint, 'LE', Math.ceil(SGValue), 'MG (' + operatorValues[dIndex].key + ')');
 
-                    if(operatorValues[dIndex].decisionVariables[j].indexOf("_SG1")>-1){
+                    if(operatorValues[dIndex].decisionVariables[j].indexOf(":::SG1")>-1){
                         innerConstraints = innerConstraints.Add(operatorValues[dIndex].decisionVariables[j], 1);
+                        innerConstraintsAdded=true;
                     }
-                    else if(operatorValues[dIndex].decisionVariables[j].indexOf("_SG2")>-1){
+                    else if(operatorValues[dIndex].decisionVariables[j].indexOf(":::SG2")>-1){
                         innerConstraints = innerConstraints.Add(operatorValues[dIndex].decisionVariables[j], -1);
+                        innerConstraintsAdded=true;
                     }
                 }
             }
-            lp.addConstraint(innerConstraints, 'LE', 0, 'SG (' + operatorValues[dIndex].key + ')');
-            lp.addConstraint(MGConstraint, 'GE', Math.floor(value), 'MG (' + operatorValues[dIndex].key + ')');
-            lp.addConstraint(MGConstraint, 'LE', Math.ceil(value), 'MG (' + operatorValues[dIndex].key + ')');
+            if(innerConstraintsAdded==true){
+                lp.addConstraint(innerConstraints, 'LE', 0, 'SG (' + operatorValues[dIndex].key + ')');
+            }
+            if(MGConstraintAdded == true){
+                lp.addConstraint(MGConstraint, 'GE', Math.floor(value), 'MG (' + operatorValues[dIndex].key + ')');
+                lp.addConstraint(MGConstraint, 'LE', Math.ceil(value), 'MG (' + operatorValues[dIndex].key + ')');
+            }
         }
         lp.addConstraint(operatorConstraint[dIndex], 'EQ', operatorValues[dIndex].supply, 'supply (' + operatorValues[dIndex].key + ')');
     } else {
@@ -150,144 +227,22 @@ for (var dIndex = 0; dIndex < demandMGKeys.length; dIndex++) {
     }
 }
 
-//var operatorIndex = operatorKeys.indexOf(supply[i][0]+'_'+supply[i][1]);
-// var operatorConstraint = [];
-// for (var iMG = 1; iMG < 4; iMG++) {
-//     for (var dIndex = 0; dIndex < operatorKeys.length; dIndex++) {
-//         var operatorSG=[];
-//         for (var j = 0; j < operatorValues[dIndex].decisionVariables.length; j++) {
-//             if(operatorValues[dIndex].decisionVariables[j].indexOf("_MG"+iMG) > -1){
-//                 operatorSG.push(operatorValues[dIndex].decisionVariables[j]);
-//             }
-//         }
-//         if(operatorSG.length>0){
-//             operatorConstraint.push({decisionVariables: operatorSG, demand:operatorValues[dIndex].supply});
-//         }
-//     }
-// }
-
-// console.log(operatorConstraint);
-
-// for (var iPanelty = 1; iPanelty < operatorConstraint.length; iPanelty++) {
-//     // for (var iSubPanelty = 0; iSubPanelty < operatorConstraint[iPanelty-1].decisionVariables.length; iSubPanelty++) {
-//     //     var val = (iPanelty%2 == 0?1:1)/operatorConstraint[iPanelty-1].demand;
-//     //     objective = objective.Add(operatorConstraint[iPanelty-1].decisionVariables[iSubPanelty], val);
-//     //     console.log(operatorConstraint[iPanelty-1].decisionVariables[iSubPanelty], val);
-//     // }
-//     // for (var iSubPanelty = 0; iSubPanelty < operatorConstraint[iPanelty].decisionVariables.length; iSubPanelty++) {
-//     //     var val = (iPanelty%2 == 0?-1:-1)/operatorConstraint[iPanelty].demand;
-//     //     objective = objective.Add(operatorConstraint[iPanelty].decisionVariables[iSubPanelty], val);
-//     //     console.log(operatorConstraint[iPanelty].decisionVariables[iSubPanelty], val);
-//     // }
-// }
-
 //console.log("objective", objective);
 lp.setObjective(objective);
 
-console.log(lp.dumpProgram());
-console.log(lp.solve());
+// console.log(lp.dumpProgram());
+var modelResult = lp.solve();
+console.log(modelResult);
 console.log('objective =', lp.getObjectiveValue());
 
-var rows=[];
-rows.push("Cluster,Operator,Part1,Part2,Allocation");
-for (var dIndex = 0; dIndex < decisionVariables.length; dIndex++) {
-    console.log(decisionVariables[dIndex].key,' =', lp.get(decisionVariables[dIndex].key));
-    var keys = decisionVariables[dIndex].key.split("_");
-    keys.push(lp.get(decisionVariables[dIndex].key));
-    rows.push(keys.join());
-}
-fs.writeFileSync("output.csv", rows.join("\r\n"));
-
-//console.log(decisionVariables);
-
-//
-// var x = lp.addColumn('x', true); // lp.addColumn('x', true) for integer variable
-// var y = lp.addColumn('y', true); // lp.addColumn('y', false, true) for binary variable
-//
-//
-// var objective = new Row().Add(x, 1).Add(y, 1);
-//
-// lp.setObjective(objective);
-
-
-// lp.addConstraint(machineatime, 'LE', 2400, 'machine a time')
-//
-// var machinebtime = new Row().Add(x, 30).Add(y, 33);
-// lp.addConstraint(machinebtime, 'LE', 2100, 'machine b time')
-//
-// lp.addConstraint(new Row().Add(x, 1), 'GE', 75 - 30, 'meet demand of x')
-// lp.addConstraint(new Row().Add(y, 1), 'GE', 95 - 90, 'meet demand of y')
-//
-// console.log(lp.dumpProgram());
-// console.log(lp.solve());
-// console.log('objective =', lp.getObjectiveValue())
-// console.log('x =', lp.get(x));
-// console.log('y =', lp.get(y));
-// console.log('machineatime =', lp.calculate(machineatime));
-// console.log('machinebtime =', lp.calculate(machinebtime));
-
-function readCsvFile(path, fieldsLength, convertUpper){
-    var data = fs.readFileSync(path);
-    var rows = data.toString().split("\r\n")
-    for (var rIndex = 0; rIndex < rows.length; rIndex++) {
-        if(rows[rIndex].length>0){
-            rows[rIndex]=rows[rIndex].split(",");
-            if(rows[rIndex].length != fieldsLength){
-                console.log(fieldsLength," fields required. Found: ",rows[rIndex].length, "Data: ", rows[rIndex]);
-                rows.splice(rIndex, 1);
-                rIndex--;
-            }
-            else {
-                if(convertUpper!=false){
-                    for (var fIndex = 0; fIndex < rows[rIndex].length; fIndex++) {
-                        rows[rIndex][fIndex] = rows[rIndex][fIndex].toUpperCase();
-                    }
-                }
-            }
-        }
-        else {
-            rows.splice(rIndex, 1);
-            rIndex--;
-        }
+if(modelResult.code === 0){
+    var rows=[];
+    rows.push("Cluster,Operator,Part1,Part2,Allocation");
+    for (var dIndex = 0; dIndex < decisionVariables.length; dIndex++) {
+        console.log(decisionVariables[dIndex].key,' =', lp.get(decisionVariables[dIndex].key));
+        var keys = decisionVariables[dIndex].key.split(":::");
+        keys.push(lp.get(decisionVariables[dIndex].key));
+        rows.push(keys.join());
     }
-    return rows;
-}
-
-function JSON2CSV(objArray, includeHeader) {
-    var array = typeof objArray != 'object' ? [objArray] : objArray;
-    //console.log(typeof objArray);
-    var str = '';
-    var tempData = [];
-    if (array.length > 0) {
-        var keys = Object.keys(array[0]);
-        if (includeHeader != false) {
-            //str += keys.join(',') + '\r\n';
-            tempData.push(keys.join(','));
-        }
-        //   console.log('JSON2CSV called for Array Size:', objArray.length, ' and keys size: ', keys.length);
-        //append data
-        for (var k = 0; k < array.length; k++) {
-            var line = [];
-            for (var index = 0; index < keys.length; index++) {
-                if (array[k].hasOwnProperty(keys[index])) {
-                    var val = array[k][keys[index]];
-                    if (typeof val == 'string' && val != null) {
-                        if (val.indexOf(',') != -1)
-                            line.push('"' + val + '"');
-                        else
-                            line.push(val);
-                    }
-                    else if (Object.prototype.toString.call(val) === '[object Date]') {
-                        line.push(dateTimeFormat.formatDate(val, "yyyy-MM-dd HH:mm:ss"));
-                    }
-                    else {
-                        line.push(val);
-                    }
-                }
-            }
-            //str += line.join(',') + '\r\n';
-            tempData.push(line.join(','));
-        }
-        return tempData.join('\r\n') + '\r\n';
-    }
+    fs.writeFileSync("output.csv", rows.join("\r\n"));
 }
